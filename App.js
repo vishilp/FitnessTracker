08c026a14bafadfb6f0a8f276b6928f703c33561
vishilp <vishilthe3rd@gmail.com>
2024-09-10 14:49:38 -0700

    created singular exercise container

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
 
 export default function App() {
+  const exercise = exercises[0]
+  console.log(exercise);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={styles.exerciseContainer}>
+      <Text style= {styles.exerciseName}>{exercise.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        {exercise.muscle.charAt(0).toUpperCase()+ exercise.muscle.slice(1)} | {exercise.equipment.charAt(0).toUpperCase()+ exercise.equipment.slice(1)}
+      </Text>
+    </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding:10,
+  },
+
+  exerciseName: {
+    fontSize: 20, 
+    fontWeight: '600',
+    
+  },
+
+  exerciseSubtitle:{
+    color: 'dimgray',
+  },
+
+  exerciseContainer:{
+    backgroundColor:'#fff',
+    padding:10,
+    borderRadius: 10,
+    gap: 5,
   },
 });
